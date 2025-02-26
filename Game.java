a98fa114511f41f4ac5e0243309f926fecdfb6d7
aidant11 <aidant10@nycstudents.net>
2025-02-26 16:49:05 +0000

    123

---
+++ Game.java
@@ -0,0 +1,29 @@
+public class Game 
+{
+private Level levelOne;
+private Level levelTwo;
+private Level levelThree;
+private boolean bonus;
+
+public Game()
+{
+
+}
+
+public boolean isBonus()
+{
+    return bonus;
+}
+
+public void makeBonus()
+{
+    bonus=true;
+}
+    
+    if (i==1) return levelOne;
+    if (i==2) return levelTwo;
+    if (i==3) return levelThree;
+
+
+}
+
