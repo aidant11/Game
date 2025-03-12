5593ed1acb93b99916047cfbb4b1302f230bf348
aidant11 <aidant10@nycstudents.net>
2025-03-12 15:48:58 +0000

    123

--- Level.java
+++ Level.java
@@ -1,29 +1,21 @@
-public class Level 
-{
+public class Level {
     private boolean goalReached;
     private int points;
-public boolean goalReached()
-{
-    return goalReached;
-}
 
-public void reachGoal()
-{
-    goalReached=true;
-}
+    public boolean goalReached() {
+        return goalReached;
+    }
 
-public int getPoints() 
-{
-    return points;
-}
+    public void reachGoal() {
+        goalReached = true;
+    }
 
-public void setPoints(int p)
-{
-    points=p;
-}
+    public int getPoints() {
+        return points;
+    }
 
-
-
-
+    public void setPoints(int p) {
+        points = p;
+    }
 
 }
